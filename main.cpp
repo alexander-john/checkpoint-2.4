c38d34ca0d31ec9e94fd3b863873ebda2f2f5ee5
Alexander John <alexjohnpersonals@gmail.com>
2022-07-03 23:03:27 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// 03 July 2022
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Alexander John" << endl;
+    cout << "123 Sorry Lane" << endl;
+    cout << "RockLine, NY 12345" << endl;
+    cout << "123 234 3456" << endl;
+    return 0;
+}
